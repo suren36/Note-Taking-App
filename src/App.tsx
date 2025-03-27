cb4bc4c8287b0747d4f64d5fb2befddc47710970
suren36 <79780614+suren36@users.noreply.github.com>
2025-03-27 15:46:34 +0545

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+import { AllRoutes } from './routes/AllRoutes.js';
+
+
+
+
+
+function App() {
+
+  return (
+    <>
+
+  <AllRoutes/>
+
+
+
+
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/NewNote.tsx
@@ -0,0 +1,15 @@
+
+import { FormNote } from '../components/FormNote'
+export const NewNote   = () => {
+  return (
+    <div className='container mx-auto p-5 border-purple-200 border-2 rounded-lg mt-10'>
+<h1 className='mb-4 text-center text-6xl text-purple-950'>
+
+   Add Notes
+</h1>
+
+<FormNote  />
+
+    </div> 
+  )
+}
